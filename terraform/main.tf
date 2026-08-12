a7a80ae4a8b43b8f9b1d725fe121e5621f709d7b
Hrishikesh Dherange <hrishikeshdherange@gmail.com>
2026-08-12 11:38:19 +0530

    feat(terraform): add branch policy to block direct push to main branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.15.0"  
+  required_version = ">= 1.5.0"
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
@@ -14,7 +14,7 @@ provider "azuredevops" {
 
 
 resource "azuredevops_project" "test" {
-  name = "demo-tflint"
+  name               = "demo-tflint"
   visibility         = "private" # added 2 intentionally for tflint to find out - there's no official plugin for azure devops for tf lint can't
   version_control    = "Git"
   work_item_template = "Agile"
@@ -24,3 +24,31 @@ resource "azuredevops_project" "test" {
     artifacts = "disabled"
   }
 }
+
+resource "azuredevops_git_repository" "repo" {
+  project_id = azuredevops_project.test.id
+  name       = "demo-repo"
+  initialization {
+    init_type = "Clean"
+  }
+}
+
+# Branch policy enforcing pull requests with minimum 1 reviewer on main branch, blocking direct pushes
+resource "azuredevops_branch_policy_min_reviewers" "block_direct_push_main" {
+  project_id = azuredevops_project.test.id
+
+  enabled  = true
+  blocking = true
+
+  settings {
+    reviewer_count     = 1
+    submitter_can_vote = false
+
+    scope {
+      repository_id  = azuredevops_git_repository.repo.id
+      repository_ref = "refs/heads/main"
+      match_type     = "Exact"
+    }
+  }
+}
+
